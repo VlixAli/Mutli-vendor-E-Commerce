1cf276d620b63977ac6cc00eb4be59e3d32c72db
Ali <qali372@gmail.com>
2024-02-07 12:03:40 +0200

    feat : calling UserSeeder.php in DatabaseSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Jerry Seinfeld' ,
+            'email' => 'jerrySeinfeld@gmail.com' ,
+            'password' => Hash::make('password'),
+            'phone_number' => '912881992',
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'George Costanza' ,
+            'email' => 'georgeCostanza@gmail.com' ,
+            'password' => Hash::make('password'),
+            'phone_number' => '912881992',
+        ]);
     }
 }
