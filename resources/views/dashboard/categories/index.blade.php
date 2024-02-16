0231a568d528bfa037cc907458f638d49682edfa
Ali <qali372@gmail.com>
2024-02-16 11:02:10 +0200

    fix : create - update -delete buttons routes for Categories index.blade.php

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 
     <div class="mb-5">
-        <a href="{{ route('categories.create') }}" class="btn btn-sm btn-outline-primary">Create</a>
+        <a href="{{ route('dashboard.categories.create') }}" class="btn btn-sm btn-outline-primary">Create</a>
     </div>
 
     @if(session()->has('success'))
@@ -39,10 +39,10 @@
                 <td>{{ $category->parent_id }}</td>
                 <td>{{ $category->created_at }}</td>
                 <td>
-                    <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-sm btn-outline-success">Edit</a>
+                    <a href="{{ route('dashboard.categories.edit', $category->id) }}" class="btn btn-sm btn-outline-success">Edit</a>
                 </td>
                 <td>
-                    <form action="{{ route('categories.destroy', $category->id) }}" method="post">
+                    <form action="{{ route('dashboard.categories.destroy', $category->id) }}" method="post">
                         @csrf
                         @method('delete')
                         <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
