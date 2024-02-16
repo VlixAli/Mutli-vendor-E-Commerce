916945a4ab9af88182e62f9919d25401b52eb48b
Ali <qali372@gmail.com>
2024-02-16 11:15:15 +0200

    fix : use the right namespace for DashboardController in dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Dashboard\CategoriesController;
-use App\Http\Controllers\DashboardController;
+
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
