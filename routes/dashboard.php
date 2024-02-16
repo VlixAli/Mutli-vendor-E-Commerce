96900963f0a77b4c5e28d85ed9314e915cce6bc2
Ali <qali372@gmail.com>
2024-02-16 10:53:20 +0200

    refactor : grouping dashboard endpoints under auth middleware

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::group([
+    'middleware' => 'auth'
+], function () {
 
-Route::resource('/dashboard/categories', CategoriesController::class)
-    ->middleware('auth');
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::resource('/dashboard/categories', CategoriesController::class);
+});
+
