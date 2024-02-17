c2dea26dd24911c3ba3ef695ca890788501881bc
Ali <qali372@gmail.com>
2024-02-17 21:12:40 +0200

    refactor : extract custom rule( forbid laravel for name field) in Category.php to Filter.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Rules\Filter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\Rule;
@@ -23,11 +24,7 @@ class Category extends Model
     {
         return ['name' => ['required', 'string', 'min:3', 'max:255',
             Rule::unique('categories', 'name')->ignore($id),
-            function ($attribute, $value, $fails) {
-                if (strtolower($value) == 'laravel') {
-                    $fails('this name is forbidden!');
-                }
-            },
+            new Filter(),
         ],
             'parent_id' => ['nullable', 'int', 'exists:categories,id'],
             'image' => ['image', 'max:1048576', 'dimensions:min_width=100,min_height=100'],
