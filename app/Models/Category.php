4aec763d516f4f83020585c410880e7e2c3a58b5
Ali <qali372@gmail.com>
2024-02-17 18:23:56 +0200

    feat : make the name field unique in category rules

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,7 +20,7 @@ class Category extends Model
 
     public static function rules()
     {
-        return ['name' => 'required|string|min:3|max:255',
+        return ['name' => 'required|string|min:3|max:255|unique',
             'parent_id' => ['nullable', 'int', 'exists:categories,id'],
             'image' => ['image', 'max:1048576', 'dimensions:min_width=100,min_height=100'],
             'status' => 'in:active,archived'
