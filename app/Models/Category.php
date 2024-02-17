c1aad4556e341af013660911bcee5fb6c09440ac
Ali <qali372@gmail.com>
2024-02-17 21:00:44 +0200

    feat : add custom rule( forbid laravel for name field) in Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,10 +22,17 @@ class Category extends Model
     public static function rules($id = 0)
     {
         return ['name' => ['required', 'string', 'min:3', 'max:255',
-            Rule::unique('categories','name')->ignore($id)],
+            Rule::unique('categories', 'name')->ignore($id),
+            function ($attribute, $value, $fails) {
+                if (strtolower($value) == 'laravel') {
+                    $fails('this name is forbidden!');
+                }
+            },
+        ],
             'parent_id' => ['nullable', 'int', 'exists:categories,id'],
             'image' => ['image', 'max:1048576', 'dimensions:min_width=100,min_height=100'],
-            'status' => 'required|in:active,archived'
+            'status' => 'required|in:active,archived',
+
         ];
     }
 }
