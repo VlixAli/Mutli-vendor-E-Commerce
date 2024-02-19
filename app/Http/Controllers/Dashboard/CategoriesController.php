36b4d587ce7012cbe1809376b8bf6077921dcfb7
Ali <qali372@gmail.com>
2024-02-19 11:17:50 +0200

    fix: stop deleting the image because we use softDeletes now in CategoriesController.php

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -130,9 +130,9 @@ class CategoriesController extends Controller
     {
         $category = Category::findOrFail($id);
         $category->delete();
-        if ($category->image) {
-            Storage::disk('public')->delete($category->image);
-        }
+//        if ($category->image) {
+//            Storage::disk('public')->delete($category->image);
+//        }
 
         return redirect()->route('dashboard.categories.index')
             ->with('success', 'Category deleted!');
