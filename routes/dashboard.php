99837ca53079276a57dea0c029c06bf9e9672c99
Ali <qali372@gmail.com>
2024-02-19 14:48:56 +0200

    feat : add products resource endpoints to dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Dashboard\CategoriesController;
 
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -22,5 +23,6 @@ Route::group([
         ->name('categories.force-delete');
 
     Route::resource('/categories', CategoriesController::class);
+    Route::resource('/products', ProductsController::class);
 });
 
