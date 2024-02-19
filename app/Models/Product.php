8c003e47edd1d60edb360b5898cb911b68a2ff23
Ali <qali372@gmail.com>
2024-02-19 16:12:10 +0200

    refactor : remove 2 unused use statements

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,8 @@
 namespace App\Models;
 
 use App\Models\Scopes\StoreScope;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
