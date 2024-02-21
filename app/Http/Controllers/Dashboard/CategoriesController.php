857a61c26ced48dbbebb223c5295b1c1d6bdc2b2
Ali <qali372@gmail.com>
2024-02-21 16:36:21 +0200

    feat : implement show() in CategoriesController.php

--- app/Http/Controllers/Dashboard/CategoriesController.php
+++ app/Http/Controllers/Dashboard/CategoriesController.php
@@ -78,9 +78,11 @@ class CategoriesController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return view('dashboard.categories.show' , [
+            'category' => $category
+        ]);
     }
 
     /**
