5f1078130f90a3095bf6e2e8cf60780fda801667
Ali <qali372@gmail.com>
2024-02-21 21:59:04 +0200

    feat : add endpoints to edit and update profile in dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Dashboard\CategoriesController;
 
 use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Dashboard\ProductsController;
+use App\Http\Controllers\Dashboard\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -11,6 +12,9 @@ Route::group([
     'as' => 'dashboard.',
     'prefix' => 'dashboard'
 ], function () {
+
+    Route::get('profile' , [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile' , [ProfileController::class, 'update'])->name('profile.update');
 
     Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
