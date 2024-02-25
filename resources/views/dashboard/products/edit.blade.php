919f8056b6e2c5ac8a971d65b42aceebb3bf9e55
Ali <qali372@gmail.com>
2024-02-25 16:56:51 +0200

    build : products edit.blade.php

--- resources/views/dashboard/products/edit.blade.php
+++ resources/views/dashboard/products/edit.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Edit Category')
+@section('title', 'Edit Product')
 
 @section('breadcrumb')
     @parent
-    <li class="breadcrumb-item active">Categories</li>
-    <li class="breadcrumb-item active">Edit Category</li>
+    <li class="breadcrumb-item active">Products</li>
+    <li class="breadcrumb-item active">Edit Product</li>
 @endsection
 
 @section('content')
 
-    <form action="{{ route('dashboard.categories.update', $category->id) }}" method="post" enctype="multipart/form-data">
+    <form action="{{ route('dashboard.products.update', $product->id) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('put')
 
-        @include('dashboard.categories._form', [
+        @include('dashboard.products._form', [
     'button_label' => 'update'
 ])
     </form>
