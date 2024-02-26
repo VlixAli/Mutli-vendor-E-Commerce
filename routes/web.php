43782edab3ebae551497d9679eb440105a6bf40d
Ali <qali372@gmail.com>
2024-02-26 20:20:36 +0200

    feat : add products index and show endpoints to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\ProductsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,15 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
-Route::get('/dash', function () {
-    return view('dashboard');
-});
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::controller(ProductsController::class)->name('products.')->group(function (){
+    Route::get('/products', 'index')->name('index');
+    Route::get('/products/{product}', 'show')->name('show');
 });
 
 require __DIR__ . '/auth.php';
