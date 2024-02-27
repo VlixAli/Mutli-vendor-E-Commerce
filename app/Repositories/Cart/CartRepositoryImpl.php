0477a78cff2cc73916adf410c72bf7851408db4b
Ali <qali372@gmail.com>
2024-02-27 19:45:27 +0200

    fix : increasing the value of a product instead of adding it again when it exists in CartRepositoryImpl.php

--- app/Repositories/Cart/CartRepositoryImpl.php
+++ app/Repositories/Cart/CartRepositoryImpl.php
@@ -22,12 +22,20 @@ class CartRepositoryImpl implements CartRepository
 
     public function add(Product $product, $quantity = 1)
     {
-        return Cart::create([
-            'cookie_id' => $this->getCookieId(),
-            'user_id' => Auth::id(),
-            'product_id' => $product->id,
-            'quantity' => $quantity,
-        ]);
+        $item = Cart::where('product_id', '=', $product->id)
+            ->cookieId($this->getCookieId())
+            ->first();
+
+        if(!$item) {
+            return Cart::create([
+                'cookie_id' => $this->getCookieId(),
+                'user_id' => Auth::id(),
+                'product_id' => $product->id,
+                'quantity' => $quantity,
+            ]);
+        }
+
+        return $item->increment('quantity',$quantity);
     }
 
     public function update(Product $product, $quantity)
