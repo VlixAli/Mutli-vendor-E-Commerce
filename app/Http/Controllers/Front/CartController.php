63b0f5de5bc51f3dad2ea0ef1d13f39e1d7cf9c8
Ali <qali372@gmail.com>
2024-02-27 19:44:47 +0200

    refactor : extract cartRepository into the constructor of CartController.php

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -11,23 +11,31 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+
+    protected CartRepository $cartRepository;
+    public function __construct(CartRepository $cartRepository)
+    {
+        $this->cartRepository = $cartRepository;
+    }
+
+
     /**
      * Display a listing of the resource.
      */
-    public function index(CartRepository $cartRepository)
+    public function index()
     {
         return view('front.cart', [
-            'cart' => $cartRepository
+            'cart' => $this->cartRepository
         ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRequest $request, CartRepository $cartRepository)
+    public function store(StoreRequest $request)
     {
         $product = Product::findOrFail($request->validated('product_id'));
-        $cartRepository->add($product, $request->validated('quantity'));
+        $this->cartRepository->add($product, $request->validated('quantity'));
 
         return redirect()->route('cart.index')->with('success' , 'Product added to cart!');
     }
@@ -35,17 +43,17 @@ class CartController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(StoreRequest $request, CartRepository $cartRepository)
+    public function update(StoreRequest $request)
     {
         $product = Product::findOrFail($request->validated('product_id'));
-        $cartRepository->update($product, $request->validated('quantity'));
+        $this->cartRepository->update($product, $request->validated('quantity'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(CartRepository $cartRepository,string $id)
+    public function destroy(string $id)
     {
-        $cartRepository->delete($id);
+        $this->cartRepository->delete($id);
     }
 }
