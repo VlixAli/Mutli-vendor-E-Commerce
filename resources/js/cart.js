c3775646f64de5e26c6e309cd8f6575533f5edce
Ali <qali372@gmail.com>
2024-02-28 06:52:09 +0200

    build : cart.js with put method

---
+++ resources/js/cart.js
@@ -0,0 +1,12 @@
+(function ($) {
+    $('.item-quantity').on('change', function (e) {
+        $.ajax({
+            url: "/cart/" + $(this).data('id'),
+            method: 'put',
+            data: {
+                quantity: $(this).val(),
+                _token: csrf_token
+            }
+        });
+    });
+})(jQuery);
