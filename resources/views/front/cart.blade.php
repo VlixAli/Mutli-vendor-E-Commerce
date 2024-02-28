d9a436c8d9ab23c0f841dbeba7f47b28ca8c68d3
Ali <qali372@gmail.com>
2024-02-28 06:53:20 +0200

    feat : add item-quantity class to input of item quantity and add needed scripts in cart.blade.php

--- resources/views/front/cart.blade.php
+++ resources/views/front/cart.blade.php
@@ -68,7 +68,7 @@
                             </div>
                             <div class="col-lg-2 col-md-2 col-12">
                                 <div class="count-input">
-                                    <input class="form-control" value="{{ $item->quantity }}">
+                                    <input class="form-control item-quantity" data-id="{{ $item->id }}" value="{{ $item->quantity }}">
                                 </div>
                             </div>
                             <div class="col-lg-2 col-md-2 col-12">
@@ -124,4 +124,12 @@
         </div>
     </div>
     <!--/ End Shopping Cart -->
+
+    @push('scripts')
+        <script>
+            const csrf_token = "{{ csrf_token() }}";
+        </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        @vite(['resources/js/cart.js'])
+    @endpush
 </x-front-layout>
