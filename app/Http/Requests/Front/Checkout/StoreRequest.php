dc869b482d915ca206a806cb7ed52b5bf72ff966
Ali <qali372@gmail.com>
2024-03-02 14:16:36 +0200

    feat : add rules to checkout StoreRequest.php

--- app/Http/Requests/Front/Checkout/StoreRequest.php
+++ app/Http/Requests/Front/Checkout/StoreRequest.php
@@ -22,7 +22,25 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-//            'first_name' =>
+            'address.billing.first_name' => 'required|string|max:255',
+            'address.billing.last_name' => 'required|string|max:255',
+            'address.billing.email' => 'required|string|max:255',
+            'address.billing.phone_number' => 'required|string|max:255',
+            'address.billing.street_address' => 'nullable|string|max:255',
+            'address.billing.city' => 'required|string|max:255',
+            'address.billing.postal_code' => 'nullable|string|max:255',
+            'address.billing.state' => 'nullable|string|max:255',
+            'address.billing.country' => 'nullable|string|max:255',
+            'address.shipping.first_name' => 'required|string|max:255',
+            'address.shipping.last_name' => 'required|string|max:255',
+            'address.shipping.email' => 'required|string|max:255',
+            'address.shipping.phone_number' => 'required|string|max:255',
+            'address.shipping.street_address' => 'nullable|string|max:255',
+            'address.shipping.city' => 'required|string|max:255',
+            'address.shipping.postal_code' => 'nullable|string|max:255',
+            'address.shipping.state' => 'nullable|string|max:255',
+            'address.shipping.country' => 'nullable|string|max:255',
+
         ];
     }
 }
