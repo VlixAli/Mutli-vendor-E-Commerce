8472d38294e1199fb938ab21b5c19e954fea341b
Ali <qali372@gmail.com>
2024-03-02 22:19:48 +0200

    fix : handle the exception when the quantity of the product is 0 in DeductProductQuantity.php (ignore it)

--- app/Listeners/DeductProductQuantity.php
+++ app/Listeners/DeductProductQuantity.php
@@ -26,8 +26,12 @@ class DeductProductQuantity
     {
         $order = $event->order;
 
-        foreach ($order->products as $product) {
-            $product->decrement('quantity', $product->order_item->quantity);
+        try {
+            foreach ($order->products as $product) {
+                $product->decrement('quantity', $product->order_item->quantity);
+            }
+        } catch (\Throwable $e){
+
         }
     }
 }
