7375a4cd0cc0b85b6fae70f078ea0b91e64746d3
Ali <qali372@gmail.com>
2024-03-04 08:34:31 +0200

    build : api ProductsController.php with its all functionality

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Product\StoreRequest;
+use App\Http\Requests\Api\Product\UpdateRequest;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $products =  Product::filter($request->query())
+            ->with('category:id,name', 'store:id,name', 'tags:id,name')
+            ->paginate();
+
+        return ProductResource::collection($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $product =  Product::create($request->validated());
+
+        return Response::json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return Response::json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Product::destroy($id);
+        return response()->json(null, 204);
+    }
+}
