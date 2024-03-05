932930be19395d7b164e1943928ebf26bc2219a7
Ali <qali372@gmail.com>
2024-03-05 08:28:39 +0200

    build : CurrencyConverterController.php with store()

---
+++ app/Http/Controllers/Front/CurrencyConverterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Front\Currency\CurrencyStoreRequest;
+use App\Services\CurrencyConverter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Session;
+
+class CurrencyConverterController extends Controller
+{
+    public function store(CurrencyStoreRequest $request)
+    {
+        $baseCurrencyCode = config('app.currency');
+        $currencyCode = $request->validated('currency_code');
+
+        $cacheKey = 'currency_rate_' .$currencyCode;
+        $rate = Cache::get($cacheKey, 0);
+
+        if (!$rate) {
+            $converter = app('currency.converter');
+            $rate = $converter->convert($baseCurrencyCode, $currencyCode);
+
+            Cache::put($cacheKey, $rate, now()->addMinutes(60));
+        }
+
+        Session::put('currency_code', $currencyCode);
+
+        return redirect()->back();
+    }
+}
