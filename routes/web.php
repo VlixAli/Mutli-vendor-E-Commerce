b145313c08770dc242e251f5401486f5d37ca925
Ali <qali372@gmail.com>
2024-03-05 21:03:21 +0200

    feat : add prefix LaravelLocalization::setLocale() to all endpoints in web.php routes file

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Front\ProductsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,26 +21,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])
-    ->name('home');
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+], function () {
+    Route::get('/', [HomeController::class, 'index'])
+        ->name('home');
 
-Route::controller(ProductsController::class)->name('products.')->group(function () {
-    Route::get('/products', 'index')->name('index');
-    Route::get('/products/{product:slug}', 'show')->name('show');
+    Route::controller(ProductsController::class)->name('products.')->group(function () {
+        Route::get('/products', 'index')->name('index');
+        Route::get('/products/{product:slug}', 'show')->name('show');
+    });
+
+    Route::resource('cart', CartController::class);
+
+    Route::controller(CheckoutController::class)->name('checkout')->group(function () {
+        Route::get('checkout', 'create');
+        Route::post('checkout', 'store');
+    });
+
+    Route::get('auth/user/2fa', [TwoFactorAuthenticationController::class, 'index'])
+        ->name('front.2fa');
+
+    Route::post('currency', [CurrencyConverterController::class, 'store'])
+        ->name('currency.store');
 });
 
-Route::resource('cart', CartController::class);
-
-Route::controller(CheckoutController::class)->name('checkout')->group(function () {
-    Route::get('checkout', 'create');
-    Route::post('checkout', 'store');
-});
-
-Route::get('auth/user/2fa', [TwoFactorAuthenticationController::class, 'index'])
-    ->name('front.2fa');
-
-Route::post('currency', [CurrencyConverterController::class, 'store'])
-    ->name('currency.store');
 
 //require __DIR__ . '/auth.php';
 require __DIR__ . '/dashboard.php';
