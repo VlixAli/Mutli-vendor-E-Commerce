f77ecaf73597e33f667da2208d1a375467876ede
Ali <qali372@gmail.com>
2024-03-05 08:29:52 +0200

    feat : get the currency from the sessions if exists and from configs if doesn't in Currency.php

--- app/Helpers/Currency.php
+++ app/Helpers/Currency.php
@@ -2,15 +2,22 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Session;
 use NumberFormatter;
 
 class Currency
 {
     public static function format($amount, $currency = null)
     {
+        $base_currency = config('app.currency', 'USD');
         $formatter = new NumberFormatter(config('app.locale'),  NumberFormatter::CURRENCY);
         if($currency == null){
-            $currency = config('app.currency', 'USD');
+            $currency = Session::get('currency_code', $base_currency);
+        }
+        if($currency !=$base_currency){
+            $rate = Cache::get('currency_rate_' . $currency, 1);
+            $amount = $amount * $rate ;
         }
         return $formatter->formatCurrency($amount, $currency);
     }
