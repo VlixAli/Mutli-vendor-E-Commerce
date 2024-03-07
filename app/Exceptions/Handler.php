e289e073d1bbbe2598ced6182f148c368dbec545
Ali <qali372@gmail.com>
2024-03-07 15:16:26 +0200

    feat : implement renderable for foreign key constraint in Exceptions Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -16,6 +19,7 @@ class Handler extends ExceptionHandler
         'current_password',
         'password',
         'password_confirmation',
+        'credit_card',
     ];
 
     /**
@@ -23,8 +27,34 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(function (QueryException $e) {
+
+            if($e->getCode() === '23000'){
+                Log::channel('sql')->warning($e->getMessage());
+                return false;
+            }
+            return true;
+        });
+
+        $this->renderable(function (QueryException $e,Request $request){
+            if($e->getCode() == 23000){
+                $message = 'Foreign key constraint failed';
+            } else {
+                $message = $e->getMessage();
+            }
+
+            if($request->expectsJson()){
+                return response()->json([
+                    'message' => $message,
+                ], 400);
+            }
+
+            return redirect()
+                ->back()
+                ->withInput()->withErrors([
+                    'message' => $e->getMessage(),
+                ])
+                ->with('info' , $message);
         });
     }
 }
