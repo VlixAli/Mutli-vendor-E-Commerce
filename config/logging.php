1175e1992614aaf9412dfc4e2781217d4ae2115a
Ali <qali372@gmail.com>
2024-03-07 15:15:32 +0200

    feat : add 'sql' log driver in logging.php

--- config/logging.php
+++ config/logging.php
@@ -58,6 +58,12 @@ return [
             'ignore_exceptions' => false,
         ],
 
+        'sql' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/sql.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
