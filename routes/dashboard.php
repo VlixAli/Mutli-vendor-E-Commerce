60786b5739847329ec83181a5a561557bd19b22c
Ali <qali372@gmail.com>
2024-03-07 11:06:44 +0200

    feat : add resource endpoints for users and admins in dashboard.php

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Dashboard\AdminsController;
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use App\Http\Controllers\Dashboard\ProductsController;
 use App\Http\Controllers\Dashboard\ProfileController;
 use App\Http\Controllers\Dashboard\RolesController;
+use App\Http\Controllers\Dashboard\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
@@ -32,6 +34,8 @@ Route::group([
         'products' => ProductsController::class,
         'categories' => CategoriesController::class,
         'roles' => RolesController::class,
+        'admins' => AdminsController::class,
+        'users' => UsersController::class,
     ]);
 });
 
