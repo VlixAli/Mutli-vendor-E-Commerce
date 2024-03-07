25cced22398616d8e1fb14838399fb339dd67f9d
Ali <qali372@gmail.com>
2024-03-07 19:32:42 +0200

    feat : add endpoints get and post for products import

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Dashboard\AdminsController;
 use App\Http\Controllers\Dashboard\CategoriesController;
 use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\ImportProductsController;
 use App\Http\Controllers\Dashboard\ProductsController;
 use App\Http\Controllers\Dashboard\ProfileController;
 use App\Http\Controllers\Dashboard\RolesController;
@@ -15,8 +16,8 @@ Route::group([
     'prefix' => 'admin/dashboard'
 ], function () {
 
-    Route::get('profile' , [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('profile' , [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
 
     Route::get('/', [DashboardController::class, 'index'])
         ->name('dashboard');
@@ -25,11 +26,13 @@ Route::group([
         ->name('categories.trash');
     Route::put('categories/{category}/restore', [CategoriesController::class, 'restore'])
         ->name('categories.restore');
-     Route::delete('categories/{category}/force-delete', [CategoriesController::class, 'forceDelete'])
+    Route::delete('categories/{category}/force-delete', [CategoriesController::class, 'forceDelete'])
         ->name('categories.force-delete');
 
-    Route::resource('/categories', CategoriesController::class);
-    Route::resource('/products', ProductsController::class);
+    Route::get('products/import', [ImportProductsController::class, 'create'])
+        ->name('products.import');
+    Route::post('products/import', [ImportProductsController::class, 'store']);
+
     Route::resources([
         'products' => ProductsController::class,
         'categories' => CategoriesController::class,
