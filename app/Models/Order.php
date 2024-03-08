fb367e06f5dca221cef5d67d353aa47b258576a9
Ali <qali372@gmail.com>
2024-03-08 16:22:47 +0200

    feat : implement orderItem and order one-to-many relationship in Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -37,6 +37,11 @@ class Order extends Model
             ]);
     }
 
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
+
     public function addresses()
     {
         return $this->hasMany(OrderAddress::class);
