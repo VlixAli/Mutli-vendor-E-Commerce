73febaca641a0a2380f226bd4bf0fb50552034a9
Ali <qali372@gmail.com>
2024-03-08 09:06:13 +0200

    feat: return all categories to front home view in HomeController.php

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,10 +13,12 @@ class HomeController extends Controller
     {
         $products = Product::with('category')->active()
             ->latest()
-            ->limit(8)
+            ->limit(21)
             ->get();
+        $categories = Category::all();
         return view('front.home', [
-            'products' => $products
+            'products' => $products,
+            'categories' => $categories
         ]);
     }
 }
