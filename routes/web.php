f5b147cf6a0a8bc002f739dbfbd447e5a4100555
Ali <qali372@gmail.com>
2024-03-08 16:21:37 +0200

    feat : payment endpoints to web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Front\CartController;
 use App\Http\Controllers\Front\CheckoutController;
 use App\Http\Controllers\Front\CurrencyConverterController;
 use App\Http\Controllers\Front\HomeController;
+use App\Http\Controllers\Front\PaymentsController;
 use App\Http\Controllers\Front\ProductsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SocialController;
@@ -53,6 +54,15 @@ Route::group([
         ->name('auth.socialite.callback');
 
     Route::get('auth/{provider}/user', [SocialController::class, 'index']);
+
+    Route::get('orders/{order}/pay', [PaymentsController::class, 'create'])
+        ->name('orders.payments.create');
+
+    Route::post('orders/{order}/stripe/payment-intent', [PaymentsController::class , 'createStripePaymentIntent'])
+        ->name('stripe.paymentIntent.create');
+
+    Route::get('orders/{order}/pay/stripe/callback', [PaymentsController::class, 'confirm'])
+        ->name('stripe.return');
 });
 
 
